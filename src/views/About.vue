315bd547dd22781af59e648e8cc4b272d5be2512
David <62397679+MacDavid1989@users.noreply.github.com>
2021-10-24 16:09:18 -0400

    add some lorem to the views, style the nav router-links

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="about">
-    About
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+      rem quam, in nesciunt itaque dicta numquam consequatur voluptas amet eaque
+      commodi ipsam iste nemo error voluptatum. At, similique impedit molestias
+      ipsum vero voluptate eaque eum sed labore dicta eius unde distinctio
+      repellat rem neque atque accusantium numquam dolor doloremque provident
+      tenetur! Blanditiis eum sequi eligendi ipsam sed quibusdam totam nostrum
+      asperiores consequuntur ab nobis, optio impedit, nemo repellat itaque
+      earum. Nihil, asperiores. Non officia recusandae molestiae et. Obcaecati,
+      illum sequi minus provident eius molestiae quibusdam exercitationem sunt
+      iste quam amet saepe odit enim, libero consequatur ratione maiores
+      similique. Perferendis, rerum.
+    </p>
   </div>
 </template>
 
 <script>
-export default {
-  name: "About",
-};
+export default {};
 </script>
 
 <style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="home">
-    Home
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat commodi
+      nihil vitae ullam sequi minus quidem, exercitationem reiciendis alias cum
+      voluptate. Tempora officiis cum accusantium, aspernatur sapiente accusamus
+      pariatur est rem incidunt qui dolores, explicabo laboriosam, perferendis
+      molestias neque quos ut dicta blanditiis inventore necessitatibus cumque
+      iure delectus iusto deleniti! Optio doloremque laboriosam porro alias id
+      dicta accusamus autem voluptas quia, explicabo ipsam facilis
+      necessitatibus tempora sequi, placeat, architecto ipsum minus dolores.
+      Nostrum, dicta atque qui unde dignissimos dolorem molestias suscipit
+      fugiat nihil dolorum optio? Amet aliquid voluptatum dicta ducimus, qui,
+      consequuntur error cumque ullam laboriosam, alias excepturi tempora illum.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat commodi
+      nihil vitae ullam sequi minus quidem, exercitationem reiciendis alias cum
+      voluptate. Tempora officiis cum accusantium, aspernatur sapiente accusamus
+      pariatur est rem incidunt qui dolores, explicabo laboriosam, perferendis
+      molestias neque quos ut dicta blanditiis inventore necessitatibus cumque
+      iure delectus iusto deleniti! Optio doloremque laboriosam porro alias id
+      dicta accusamus autem voluptas quia, explicabo ipsam facilis
+      necessitatibus tempora sequi, placeat, architecto ipsum minus dolores.
+      Nostrum, dicta atque qui unde dignissimos dolorem molestias suscipit
+      fugiat nihil dolorum optio? Amet aliquid voluptatum dicta ducimus, qui,
+      consequuntur error cumque ullam laboriosam, alias excepturi tempora illum.
+    </p>
   </div>
 </template>
 
